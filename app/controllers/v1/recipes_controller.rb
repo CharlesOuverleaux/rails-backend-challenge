fcfb45be732303994107cece8c782d672fbbe3cf
Charles Ouverleaux <33698505+CharlesOuverleaux@users.noreply.github.com>
2021-11-04 16:57:34 +0100

    Added recipes API (source Contentful)

--- app/controllers/v1/recipes_controller.rb
+++ app/controllers/v1/recipes_controller.rb
@@ -1,9 +1,40 @@
+require 'contentful'
+
 class V1::RecipesController < ApplicationController
   def index
-    # @recipes = Recipe.all
+    render json: get_recipes
   end
 
   def show
     # @recipe = Recipe.find(params[:id])
   end
+
+  private
+
+  def contentful_client
+    @client = Contentful::Client.new(
+      space: 'kk2bw5ojx476',
+      access_token: '7ac531648a1b5e1dab6c18b0979f822a5aad0fe5f1109829b8a197eb2be4b84c',
+      environment: 'master',
+      dynamic_entries: :auto
+    )
+  end
+
+  def get_recipes_from_client
+    contentful_client.entries('sys.contentType.sys.id' => 'recipe')
+  end
+
+  def get_recipes
+    get_recipes_from_client.map do |recipe|
+      {
+        id: recipe.id,
+        title: recipe.title,
+        image: recipe.photo.url,
+        tags: recipe.fields[:tags] ? recipe.tags.map { |tag| tag.name } : [],
+        description: recipe.description,
+        chef: recipe.fields[:chef] ? recipe.chef.name : ''
+      }
+    end
+  end
+
 end
